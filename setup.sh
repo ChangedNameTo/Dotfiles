b4b26c1d7025badade344981e753a73bfabc39d4
ChangedNameTo <william.m95@gmail.com>
2016-12-24 22:15:27 -0500

    Made a ton of additions to my .vimrc

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 main() {
-	zsh
-	vim
+	setup_zsh
+	setup_vim
 }
 
 #############
 # ZSH SETUP #
 #############
-zsh() {
+setup_zsh() {
     $BIN_ECHO " Starting zsh installation\n"
 	# First installs oh-my-zsh
 
@@ -67,7 +67,7 @@ zsh() {
 #############
 # VIM SETUP #
 #############
-vim() {
+setup_vim() {
     $BIN_ECHO " Starting setup of vim\n"
 
     # Installs vim plug
@@ -78,10 +78,7 @@ vim() {
 	# Installs vim pathogen
     $BIN_ECHO " Installing vim pathogen\n"
 	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-    # Prompts the user to run :PlugInstall so they don't get a ton of warnings
-    vim postinstall
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
     # Move to directory of script
     $BIN_ECHO " Backing up and updating the .vimrc\n"
@@ -96,6 +93,25 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 	# Create hardlinks between local vimrc files and the actual vimrc files
 	ln -f .vimrc ~/
+
+    # Prompts the user to run :PlugInstall so they actually get to use those plugins
+    vim postinstall
+
+    # Theres a bunch of stuff that requires pathogen. I'm putting it here
+    $BIN_ECHO " Installing the plugins that use pathogen\n"
+
+    $BIN_ECHO " Installing vim-ruby\n"
+    git clone git://github.com/vim-ruby/vim-ruby.git ~/.vim/bundle/vim-ruby
+
+	$BIN_ECHO " Installing YouCompleteMe\n"
+	if [ -f /etc/debian_version ];  then
+		sudo apt-get install build-essential cmake
+		sudo apt-get install python-dev python3-dev
+		cd ~/.vim/bundle/YouCompleteMe
+		./install.py --all
+	else
+		$BIN_ECHO " Since this isn't a debian system, YouCompleteMe installation isn't supported by this script.\n"
+	fi
 
 	# Return to original dir
 	cd $OLDDIR
